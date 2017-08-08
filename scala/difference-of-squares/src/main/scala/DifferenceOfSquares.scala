c9169c1d4b5616bead3bceb07f334869cce17228
Martin Petluš <martinpetlus@gmail.com>
2017-08-08 22:15:46 +0200

    Implement DifferenceOfSquares

---
+++ scala/difference-of-squares/src/main/scala/DifferenceOfSquares.scala
@@ -0,0 +1,13 @@
+import scala.math.pow
+
+object DifferenceOfSquares {
+
+  def sumOfSquares(n: Int): Int =
+    ((1 to n).map(pow(_, 2)).sum).toInt
+
+  def squareOfSum(n: Int): Int =
+    (pow((1 to n).sum, 2)).toInt
+
+  def differenceOfSquares(n: Int): Int =
+    squareOfSum(n) - sumOfSquares(n)
+}
